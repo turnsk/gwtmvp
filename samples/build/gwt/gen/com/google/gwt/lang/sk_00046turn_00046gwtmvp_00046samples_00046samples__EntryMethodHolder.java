63bc63da96a6b5eb4b537a04a7412a8252fccb34
Marek Zajko <zajko.marek@gmail.com>
2018-04-05 11:29:20 +0200

    removed generated files

--- samples/build/gwt/gen/com/google/gwt/lang/sk_00046turn_00046gwtmvp_00046samples_00046samples__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class sk_00046turn_00046gwtmvp_00046samples_00046samples__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
